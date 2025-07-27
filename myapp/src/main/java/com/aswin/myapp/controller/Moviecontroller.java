a836ab8b50a1d992805bc70e5a9048da503f6e21
ASWIN S <113883776+aswinsee796@users.noreply.github.com>
2025-07-27 13:21:26 +0530

    Update Moviecontroller.java

--- myapp/src/main/java/com/aswin/myapp/controller/Moviecontroller.java
+++ myapp/src/main/java/com/aswin/myapp/controller/Moviecontroller.java
@@ -1,3 +1,31 @@
+package com.aswin.myapp.controller;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import com.aswin.myapp.entity.Movies;
+import com.aswin.myapp.entity.User;
+import com.aswin.myapp.service.Movieservice;
+import com.aswin.myapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/Movies")
