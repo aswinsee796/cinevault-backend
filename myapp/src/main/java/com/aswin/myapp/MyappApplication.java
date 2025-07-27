ee4376ca2642ddfacc182755304f29407c8b3456
ASWIN S <113883776+aswinsee796@users.noreply.github.com>
2025-07-27 10:42:37 +0530

    Update MyappApplication.java

--- myapp/src/main/java/com/aswin/myapp/MyappApplication.java
+++ myapp/src/main/java/com/aswin/myapp/MyappApplication.java
@@ -2,19 +2,20 @@ package com.aswin.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MyappApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyappApplication.class, args);
     }
+}
 
-    // This runs after Spring Boot starts
-    @PostConstruct
-    public void logPort() {
-        System.out.println("Environment PORT variable: " + System.getenv("PORT"));
+@RestController
+class HealthController {
+    @GetMapping("/")
+    public String root() {
+        return "Backend is alive!";
     }
 }
