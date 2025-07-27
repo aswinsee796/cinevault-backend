da9447da9a4b3f1fca4085c13a9ab9c5c29fa68d
ASWIN S <113883776+aswinsee796@users.noreply.github.com>
2025-07-27 10:19:22 +0530

    Update MyappApplication.java

--- myapp/src/main/java/com/aswin/myapp/MyappApplication.java
+++ myapp/src/main/java/com/aswin/myapp/MyappApplication.java
@@ -3,11 +3,18 @@ package com.aswin.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MyappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyappApplication.class, args);
+    }
 
+    // This runs after Spring Boot starts
+    @PostConstruct
+    public void logPort() {
+        System.out.println("Environment PORT variable: " + System.getenv("PORT"));
+    }
 }
