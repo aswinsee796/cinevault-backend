1f8f37f6200ca7cfbe2c8b3db758311c00316315
ASWIN S <113883776+aswinsee796@users.noreply.github.com>
2025-07-27 13:31:33 +0530

    Update HealthController.java

--- myapp/src/main/java/com/aswin/myapp/controller/HealthController.java
+++ myapp/src/main/java/com/aswin/myapp/controller/HealthController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    @GetMapping("/")
+    public String home() {
+        return "Backend running!";
+    }
+
     @GetMapping("/ping")
     public String ping() {
         return "Backend is alive!";
+    }
+
+    @GetMapping("/favicon.ico")
+    public void favicon() {
+        // Browser requests for favicon.ico will get HTTP 200 OK with no content
     }
 }
